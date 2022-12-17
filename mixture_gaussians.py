910308ceba6d51762e3cef75cebe8d8a2e3a088f
Hadar Rosenwald <hadar@sunbit.com>
2022-12-17 12:53:28 +0200

    handle value error for finding ppf

--- mixture_gaussians.py
+++ mixture_gaussians.py
@@ -37,23 +37,30 @@ class MixtureDistribution:
 
     @_vectorize_float
     def ppf(self, p):
-        # The percentage-point function inverts the cdf using the standard root_scalar function. As a cdf is monotonic, the bracketing interval must be bounded by the mininum and maximum of the quantitle function across all the component distributions.
+        # The percentage-point function inverts the cdf using the standard root_scalar function. As a cdf is monotonic, the bracketing interval must be bounded by the minimum and maximum of the quantile function across all the component distributions.
 
         if p == 0:
             return -float('Inf')  # alpha=0 -> quantile=-inf
         elif p == 1:
             return float('Inf')  # alpha=1 -> quantile=inf
         else:
-            p = round(p, 10)
             bracket = [min(dist.ppf(p) for dist in self._distributions),
                        max(dist.ppf(p) for dist in self._distributions)]
 
-            r = root_scalar(
-                f=lambda x: self.cdf(x) - p,
-                fprime=self.pdf,
-                bracket=bracket,
-                x0=0
-            )
-            if not r.converged:
-                raise ValueError("Mixture of gaussian quantiles failed to converge.")
-            return r.root
+            try:
+                r = root_scalar(
+                    f=lambda x: self.cdf(x) - p,
+                    fprime=self.pdf,
+                    bracket=bracket,
+                    x0=0
+                )
+                if not r.converged:
+                    raise ValueError("Mixture of gaussian quantiles failed to converge.")
+                return r.root
+            except ValueError as e:
+                if bracket[0] == bracket[1]:
+                    return bracket[0] # TODO THINK IF THIS IS CORRECT
+                else:
+                    raise ValueError("Mixture of gaussian quantiles failed to find a root.")
+
+
